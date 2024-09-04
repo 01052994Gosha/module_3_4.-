cc840ff8c36a9c98bfdd8352d76e4cc012d813f1
01052994Gosha <potehindv@mail.ru>
2024-09-04 13:24:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-def print_params(a=1,b='строка', c=True):
-    print(a,b,c)
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
+            same_words.append(i)
+
+    return same_words
 
 
-print_params()
-print_params(1)
-print_params(1,b='строка',c= True)
-print_params(1,b=25,c=[1,2,3])
-
-values_list = [1,'строка', True]
-values_dict = {'a': 1, 'b': 2,'c':3}
-print_params(*values_list)
-print_params(**values_dict)
-
-values_list_2= (1,True)
-print_params(*values_list_2,42)
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
